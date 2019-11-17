615175670b0c2c1c5a1d911cc77102a21ed58c01
mukulpathania96 <mukulpathania96@gmail.com>
2019-11-17 19:47:16 +0530

    Started tracking my first flask webapp.

---
+++ vsearch4web.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def hello() -> str:
+    return 'Hello world from Flask!'
+@app.route('/search4')
+def do_search() -> str:
+    return str(set('life, the universe, and everything').intersection('eiru,!'))
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+                            the_title='Welcome to search4letters on the web!')
+app.run(debug=True)
