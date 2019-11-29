24d5d7e0ec96cc558b0e55193149d710a4aa6e09
Mukul Pathania <mukulpathania96@gmail.com>
2019-11-29 13:43:46 +0530

    Enbale the app to record data and add database connection
    This commit wil enable the webapp to record data in a MYSQL table.

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,12 +1,43 @@
 from flask import Flask, render_template, request
+import mysql.connector
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'Hello world from Flask!'
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+    dbconfig = { 'host': '127.0.0.1',
+                 'user': 'vsearch',
+                 'password': 'vsearchpasswd',
+                 'database': 'vsearchlogDB', }
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = """insert into log
+           (phrase, letters, ip, browser_string, results)
+            values
+            (%s, %s, %s, %s, %s)"""
+   
+    cursor.execute(_SQL, (req.form['phrase'],
+                   req.form['letters'],
+                   req.remote_addr,
+                   req.user_agent.browser,
+                   res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
 @app.route('/search4', methods = ["POST","GET"])
 def do_search():
-    return str(set(request.form["phrase"]).intersection(set(request.form["letters"])))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results =str(set(phrase).intersection(set(letters)))
+    log_request(request,results)
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page():
     return render_template('entry.html',
